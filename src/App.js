d50ce4ee6dce75aa7524155e94f375df0f911855
Kosteyyy <averkiev@yandex.ru>
2021-07-05 14:38:12 +0300

    elements no drag-drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const MovableItem = () => { 
+  return (
+    <div className="movable-item">
+      I like to move it
+    </div>
+  )
+}
+
+const FirstColumn = () => {
+  return (
+    <div className="column first-column">
+      Column 1
+      <MovableItem />
+    </div>
+  )
+}
+
+const SecondColumn = () => {
+  return (
+    <div className="column second-column">
+      Column 2 
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FirstColumn />
+      <SecondColumn /> 
     </div>
   );
 }
